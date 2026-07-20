01e3eb85534563ba2adf627c4a1f94db91533c69
Abdullah kishwar <abdullahkishwar80@gmail.com>
2026-07-20 18:40:51 +0500

    Make navbar fully responsive

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+  apiKey: "AIzaSyCN_PgW8w0Lq3LFZHeZuiBp0TE24rm0Bs8",
+  authDomain: "level-tube.firebaseapp.com",
+  projectId: "level-tube",
+  storageBucket: "level-tube.firebasestorage.app",
+  messagingSenderId: "757894268556",
+  appId: "1:757894268556:web:32b0f6647c2bb9c9f99da9",
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
